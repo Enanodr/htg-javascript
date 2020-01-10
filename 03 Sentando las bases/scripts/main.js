e0b444dd68ce9cd5b6e18d8a3bf2a0c42f2d1ec0
Enrique <jmtov4@gmail.com>
2020-01-10 03:01:14 -0300

    Se agregaron los archivos base para el primer ejercicio del tema 03. Sentando las bases

---
+++ 03 Sentando las bases/scripts/main.js
@@ -0,0 +1,1 @@
+console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
